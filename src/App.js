d07280f3ab8f42afec8deeb3f74ecd911f5ff35b
drewkiimon <andrew.r.pagan@gmail.com>
2020-05-07 21:37:15 -0700

    npm start changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.scss";
 
 function App() {
-	return <div className='App'>Cool</div>;
+	return <div className='App'>Meow I'm a cat</div>;
 }
 
 export default App;
