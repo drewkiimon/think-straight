24b3510c5917335fd68da4131e50cc36a3ea0c0a
drewkiimon <apagan@reputation.com>
2020-05-09 10:39:33 -0700

    Timer now toggles color of button

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,38 +1,51 @@
 import React, { Component } from "react";
 import Button from '@material-ui/core/Button';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { green, red } from '@material-ui/core/colors';
 
 import "./Timer.scss";
-
 
 class Timer extends Component {
     constructor(props) {
         super(props);
 
-        this.toggleTimer = this.toggleTimer.bind(this);
-
         this.state = {
             active: false,
             pomodoroLength: 25 * 60,
-            current: 0
+            current: 0,
+            theme: createMuiTheme({
+                palette: {
+                  primary: green
+                },
+            })
         }
+
+        this.toggleTimer = this.toggleTimer.bind(this);
     }
 
     toggleTimer(event) {
-        this.setState({active: !this.state.active});
-        console.log(this.state.active)
+        this.setState(
+            {
+                active: !this.state.active,
+                theme: createMuiTheme({
+                    palette: {
+                        primary: this.state.active ? green : red
+                    },
+                })
+            }
+        );
     }
 
     render() {
 
         return (
-        <div className="timer">
-            <Button variant="contained" color="primary" onClick={this.toggleTimer}>
-                {this.state.active
-                    ? "Stop" 
-                    : "Start"
-                }
-            </Button>
-        </div>
+            <div className="timer">
+                <ThemeProvider theme={this.state.theme}>
+                    <Button variant="contained" color="primary" onClick={this.toggleTimer}>
+                        { this.state.active ? "Stop" : "Start" }
+                    </Button>
+                </ThemeProvider>
+            </div>
         )
     }
 }
