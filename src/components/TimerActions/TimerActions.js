589e5fe3ab401e6d55d227ff11433487aa2a0f6c
drewkiimon <apagan@reputation.com>
2020-05-21 13:28:31 -0700

    Added modal to remove completed pomodoros

--- src/components/TimerActions/TimerActions.js
+++ src/components/TimerActions/TimerActions.js
@@ -1,13 +1,52 @@
 import React, { useState } from "react";
 import * as Constants from "./../../constants/constants";
-import IconButton from "@material-ui/core/IconButton";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
+// import IconButton from "@material-ui/core/IconButton";
+// import Menu from "@material-ui/core/Menu";
+// import MenuItem from "@material-ui/core/MenuItem";
+import {Button, Menu, MenuItem, IconButton} from "@material-ui/core/";
+
+// For modal
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 import "./TimerActions.scss";
 
+const useStyles = makeStyles((theme) => ({
+	modal: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	paper: {
+		backgroundColor: theme.palette.background.paper,
+		border: '2px solid #000',
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+	},
+}));
+
 const TimerActions = props => {
+	const classes = useStyles();
+
 	const [isOpen, setIsOpen] = useState(null);
+	const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+	const handleOpenModal = () => {
+		setIsModalOpen(true);
+	};
+  
+	const handleCloseModal = () => {
+		console.log('yes');
+		setIsModalOpen(false);
+	};
+
+	const handleRemovePomodoros = () => {
+		console.log('Removing stuff');
+		handleCloseModal();
+		handleCloseMenu();
+	};
 
 	const toggleMenu = (event) => {
 		setIsOpen(event.currentTarget);
@@ -18,12 +57,40 @@ const TimerActions = props => {
 		setIsOpen(null);
 	}
 
-	const handleClose = () => {
+	const handleCloseMenu = () => {
 		setIsOpen(null);
 	}
 
 	return (
 		<div className='timer-actions'>
+			<Modal
+				aria-labelledby="transition-modal-title"
+				aria-describedby="transition-modal-description"
+				className={classes.modal}
+				open={isModalOpen}
+				onClose={handleCloseModal}
+				closeAfterTransition
+				BackdropComponent={Backdrop}
+				BackdropProps={{
+				timeout: 500,
+				}}
+			>
+				<Fade in={isModalOpen}>
+					<div className={classes.paper}>
+						<h2 id="transition-modal-title">Remove Today's Completed Pomodoros</h2>
+						<p id="transition-modal-description">This cannot be undone</p>
+
+						<Button
+							className='timer-action-button'
+							variant='contained'
+							color='primary'
+							onClick={handleRemovePomodoros}>
+							Remove
+						</Button>
+					</div>
+				</Fade>
+			</Modal>
+
 			<IconButton
 				className='timer-actions-button'
 				aria-controls='simple-menu'
@@ -36,9 +103,12 @@ const TimerActions = props => {
 				anchorEl={isOpen}
 				keepMounted
 				open={Boolean(isOpen)}
-				onClose={handleClose}>
+				onClose={handleCloseMenu}>
 				<MenuItem onClick={stopPomodoroCycle}>
 					{Constants.RESET}
+				</MenuItem>
+				<MenuItem onClick={handleOpenModal}>
+					Remove pls
 				</MenuItem>
 			</Menu>
 		</div>
